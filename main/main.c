e4a9b325bcd7fe39f9d8d2b387b4e8e8e3427b9f
lurk101 <lurkone0one@gmail.com>
2022-05-31 16:03:57 -0400

    deepsleep

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_netif.h"
+#include "esp_sleep.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 
@@ -63,6 +64,8 @@ static void mqtt_event_handler(void* handler_args, esp_event_base_t base, int32_
         break;
     case MQTT_EVENT_PUBLISHED:
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        esp_sleep_enable_timer_wakeup(15 * 60 * 100);
+        esp_deep_sleep_start();
         break;
     case MQTT_EVENT_DATA:
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
